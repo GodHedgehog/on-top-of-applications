3d3be7d003b293d76f967227f14aba5af890388e
GodHedgehog <iezov298@gmail.com>
2025-09-27 08:46:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,29 @@ class OverlayButtonApp(App):
         return layout
 
     def perform_swipe(self, instance):
-        # Получаем доступ к нашей службе
-        service = autoclass('org.test.swipeservice.ServiceSwipeService')
+        # Импортируем PythonActivity для доступа к контексту Android
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        mActivity = PythonActivity.mActivity
+
+        # Динамически получаем имя пакета текущего приложения
+        package_name = mActivity.getPackageName()
+
+        # Формируем правильное и полное имя класса службы
+        # "Service" + "SwipeService" (имя из buildozer.spec)
+        service_class_name = f'{package_name}.ServiceSwipeService'
         
-        # Вызываем метод для выполнения свайпа
-        if service.getInstance():
-            service.getInstance().swipeUp()
+        try:
+            # Получаем доступ к классу нашей службы
+            service = autoclass(service_class_name)
+            
+            # Вызываем метод для выполнения свайпа
+            if service.getInstance():
+                service.getInstance().swipeUp()
+            else:
+                print("SwipeService: Service instance is not available.")
+        except Exception as e:
+            print(f"SwipeService: Could not find or access service class: {e}")
 
 if __name__ == '__main__':
     OverlayButtonApp().run()
+
